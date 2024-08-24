ee65358773b9eb80938b16000ca6539cf183b0da
ofmi <goncarm354@gmail.com>
2024-08-25 00:01:48 +0300

    Feedback Form

---
+++ FeedbackForm/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_FeedbackForm_FILE D:/QtMiniProjects/FeedbackForm/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/FeedbackForm.exe)
+set(__QT_DEPLOY_TARGET_FeedbackForm_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_FeedbackForm_RUNTIME_DLLS D:/Qt/6.7.2/mingw_64/bin/Qt6Widgets.dll;D:/Qt/6.7.2/mingw_64/bin/Qt6Gui.dll;D:/Qt/6.7.2/mingw_64/bin/Qt6Core.dll)
