c26aaafa9b74b90a2bac82080431d8ea43786b53
ofmi <goncarm354@gmail.com>
2024-09-07 23:00:03 +0300

    Remove all build files from ToDoList

--- ToDoList/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_ToDoList_FILE D:/QtMiniProjects/ToDoList/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/ToDoList.exe)
-set(__QT_DEPLOY_TARGET_ToDoList_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_ToDoList_RUNTIME_DLLS D:/Qt/6.7.2/mingw_64/bin/Qt6Widgets.dll;D:/Qt/6.7.2/mingw_64/bin/Qt6Sql.dll;D:/Qt/6.7.2/mingw_64/bin/Qt6Gui.dll;D:/Qt/6.7.2/mingw_64/bin/Qt6Core.dll)
