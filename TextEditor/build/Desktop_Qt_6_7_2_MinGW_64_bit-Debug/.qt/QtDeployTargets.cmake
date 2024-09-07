e3062022ce1c12a1ddfca6c4688b6aaccf3b5cec
ofmi <goncarm354@gmail.com>
2024-09-07 23:42:07 +0300

    Add executable file and dependencies

--- TextEditor/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_TextEditor_FILE D:/QtMiniProjects/TextEditor/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/TextEditor.exe)
-set(__QT_DEPLOY_TARGET_TextEditor_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_TextEditor_RUNTIME_DLLS D:/Qt/6.7.2/mingw_64/bin/Qt6Widgets.dll;D:/Qt/6.7.2/mingw_64/bin/Qt6Gui.dll;D:/Qt/6.7.2/mingw_64/bin/Qt6Core.dll)
