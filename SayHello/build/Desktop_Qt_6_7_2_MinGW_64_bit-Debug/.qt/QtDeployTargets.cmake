1ff60c79ca4de67e24161c314834036140f2c2f6
ofmi <goncarm354@gmail.com>
2024-09-07 23:58:02 +0300

    Remove files from tracking and add to .gitignore

--- SayHello/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_SayHello_FILE D:/QtMiniProjects/SayHello/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/SayHello.exe)
-set(__QT_DEPLOY_TARGET_SayHello_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_SayHello_RUNTIME_DLLS D:/Qt/6.7.2/mingw_64/bin/Qt6Widgets.dll;D:/Qt/6.7.2/mingw_64/bin/Qt6Gui.dll;D:/Qt/6.7.2/mingw_64/bin/Qt6Core.dll)
